b38835c73b74aaccc05e4cbb2d29cf0a6f3f601a
DuongYenAnh <johndoe@example.com>
2020-06-12 16:42:51 +0700

    push code giao dien log in

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Login</h2>
+
+    <form>
+    <div class="form-login">
+        <label for="Email" class="form-label-email">Email</label>
+        <input type="email" class="form-control" id="inputEmail" placeholder="Email"> 
+  
+        <label for="Password" class="form-label-password">Password</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+        
+        <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+
+</form>
+  </div>
+</template>
+
+<script>
+
+export default {
+    
+    data(){
+        return{
+        email:'',
+        password:'',
+        }
+        
+    },
+}
+</script>
