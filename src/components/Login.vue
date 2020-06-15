905acf8da655f0b1c09c340937cf4b6318ef24e6
Yen Anh <yenanh@Yens-MacBook-Pro.local>
2020-06-15 14:33:27 +0700

    dev-ya day data login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,30 +3,42 @@
     <h2>Login</h2>
 
     <form>
-    <div class="form-login">
+      <div class="form-login">
         <label for="Email" class="form-label-email">Email</label>
-        <input type="email" class="form-control" id="inputEmail" placeholder="Email"> 
-  
-        <label for="Password" class="form-label-password">Password</label>
-        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-        
-        <button type="submit" class="btn btn-primary">Sign in</button>
-  </div>
+        <input type="text" class="form-control" id="inputEmail" placeholder="Email" v-model="email" />
 
-</form>
+        <label for="Password" class="form-label-password">Password</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password" v-model="password" />
+
+        <button type="button" class="btn btn-primary" @click='login'>Sign in</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-
+import axios from "axios";
 export default {
-    
-    data(){
-        return{
-        email:'',
-        password:'',
-        }
-        
-    },
-}
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post("http://localhost:3000/auth/login", {
+          email:this.email,
+          password: this.password
+        })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
 </script>
